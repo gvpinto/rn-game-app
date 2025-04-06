2ecc46e48af05807c9fe5b1e22f0196fb16febb0
Glenn Pinto <gvpinto@gmail.com>
2025-04-06 18:35:25 -0400

    Section 4: Diving deeper into Components, Layouts and Styling - Building a Mini-Game App - Lesson 48: Setting up our screen components

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, TextInput, View } from 'react-native';
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+export default StartGameScreen;
