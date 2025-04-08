fa367f69fdc653184506d8e2d2a9e23f0a5211f2
Glenn Pinto <gvpinto@gmail.com>
2025-04-08 19:43:54 -0400

    Section 3: Diving deeper into components layouts & styling - Building a mini game-app - Lesson 51: Styling the number input element

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,42 @@
 import { StyleSheet, TextInput, View } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: '#72063c',
+    elevation: 4, // Android shadow only
+    shadowColor: 'black', // iOS shadow color
+    shadowOffset: { width: 0, height: 2 }, // iOS shadow offset
+    shadowRadius: 6, // iOS shadow radius
+    shadowOpacity: 0.25, // iOS shadow opacity
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    paddingBottom: 0,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+  },
+});
 
 export default StartGameScreen;
